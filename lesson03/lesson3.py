2d3d8be8ebfd57eec9fd760794a6de1137c8f5b3
PRABHAV PRAKASH <prabhavprakash@sea-b5d06j3.edu.yrdsb.net>
2025-02-14 14:00:07 -0500

    solves lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,9 @@
 # Lesson 3
+tiles = input("Enter your number of tiles: ")
+tiles = int(tiles)
+
+calc = int((tiles ** 0.5) //1)
+
+print (f"The maximum side length is: {calc}" ) 
+
+# print ("The maximum side length is: ", calc)   
