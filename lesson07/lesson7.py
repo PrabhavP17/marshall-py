818c7d35578610f2b80974ae0ef3f7e1cd97b7e2
Prabhav Prakash <prabhavprakash66@gmail.com>
2025-02-19 14:53:41 -0500

    solved problem 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,16 @@
 # Lesson 7
+import random
+dc_user = int(input("Enter the DC level: "))
+dice  = random.randrange(1, 21)
+print(f"Abilityt: {dice}")
+
+if dc_user <= 20:
+    if dice >= dc_user:
+        print("You Win")
+
+    else: 
+        print("You Lose")
+else:
+    print("Invalid DC")
+
+
