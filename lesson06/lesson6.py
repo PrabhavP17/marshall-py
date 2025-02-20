84925c506db530473467b7f85fb931b22ce1eada
Prabhav Prakash <prabhavprakash66@gmail.com>
2025-02-20 14:59:50 -0500

    solved CCC practice problems

--- lesson06/lesson6.py
+++ lesson06/lesson6.py
@@ -1,1 +1,50 @@
 # Lesson 6
+
+# problem 1
+# # user_input = [int(x) for x in input("Enter the measurements of the two paintings: ").split()]
+
+# # w1,h1,w2,h2 = user_input
+
+# # # if w1 == w2:
+# # #     if h1 == h2:
+# # #         all_legths_equal = int(2*((w1+w2)+h1))
+# # #         print (all_legths_equal)
+
+# # # else:
+# # #     print ("false")
+
+# # # side by siode 
+# # perimeter1 = 2*(w1+w2) + 2*max(h1,h2)
+
+# # # top and bottom
+# # perimeter2 = 2*(h1+h2) + 2*max(w1,w2)
+
+# # # Final Answer
+# # print(f"Minimum perimeter is: {min(perimeter1, perimeter2)}")
+
+# problem 2 
+
+# # num = int(input("Enter a number: "))
+
+# # counter = 1
+# # total = 0
+# # while counter < num:
+    
+# #     if num % counter == 0:
+# #         total += counter
+# #         print(counter)
+# #     counter += 1
+
+    
+# # if total == num:
+# #     print(f"{num} is a perfect number")
+
+# # elif total < num:
+# #     print(f"{num} is a deficient number")
+
+# # else:
+# #     print(f"{num} is an abundant number")
+
+
+
+
